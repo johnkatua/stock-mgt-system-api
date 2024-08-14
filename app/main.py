a2599ce8333bbb9440dbafdbaa81ddb3340ee327
johnkatua <johnkatua99@gmail.com>
2024-08-14 18:24:34 +0300

    refactor main file

--- app/main.py
+++ app/main.py
@@ -1,27 +1,11 @@
-from fastapi import Depends, FastAPI, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from datetime import datetime, timedelta
-from jose import JWTError, jwt
-from passlib.context import CryptContext
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from dotenv import dotenv_values
 
-config = dotenv_values(".env")
-
-mongo_uri = config["MONGO_URI"]
+config = dotenv_values()
 
 app = FastAPI()
 
 @app.get("/api/healthcheck")
 def root():
   return {"message": "Welcome"}
-
-
-client = MongoClient(mongo_uri)
-
-try:
-  client.admin.command('ping')
-  print('pinged successfully')
-except Exception as e:
-  print(e)
