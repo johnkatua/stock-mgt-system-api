333270f0c62524d3c2d865099c264f572655a8b8
johnkatua <johnkatua99@gmail.com>
2024-08-14 14:48:00 +0300

    moved everything to the app folder

---
+++ app/main.py
@@ -0,0 +1,28 @@
+import os
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+
+mongo_uri = config["MONGO_URI"]
+
+app = FastAPI()
+
+@app.get("/api/healthcheck")
+def root():
+  return {"message": "Welcome"}
+
+
+client = MongoClient(mongo_uri)
+
+try:
+  client.admin.command('ping')
+  print('pinged successfully')
+except Exception as e:
+  print(e)
