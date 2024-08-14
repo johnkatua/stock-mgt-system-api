b7b7bd7e6192fdf6f0a4df9290ea84a8e1d0f697
johnkatua <johnkatua99@gmail.com>
2024-08-14 13:07:03 +0300

    app health check endpoint

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 app = FastAPI()
+
+@app.get("/")
+def health_check():
+  return {"hello": "world"}
