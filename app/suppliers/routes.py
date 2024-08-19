45c0e08c492589fe81a6c7ec08da624b9e102e3b
johnkatua <johnkatua99@gmail.com>
2024-08-19 14:38:25 +0300

    add await on db calls

--- app/suppliers/routes.py
+++ app/suppliers/routes.py
@@ -12,7 +12,7 @@ router = APIRouter()
 async def create_supplier(payload: SupplierSchema = Body(...)):
   supplier = jsonable_encoder(payload)
   try:
-    Supplier.insert_one(supplier)
+    await Supplier.insert_one(supplier)
     return JSONResponse(
       status_code=HttpStatus.CREATED,
       data=jsonable_encoder({
@@ -30,11 +30,43 @@ async def create_supplier(payload: SupplierSchema = Body(...)):
 @router.get('/', response_description="List all suppliers", response_model=List[SupplierSchema])
 async def list_suppliers():
   try:
-    suppliers = list(Supplier.find(limit=100))
+    suppliers = await list(Supplier.find(limit=100))
     return JSONResponse(
       status_code=HttpStatus.OK,
       data=suppliers
     )
+  except Exception as e:
+    return JSONResponse(
+      status_code=HttpStatus.SERVER_ERROR,
+      data=jsonable_encoder({
+        "msg": str(e)
+      })
+    )
+
+@router.put('/id', response_description="Update a supplier", response_model=SupplierSchema)
+async def update_supplier(id: str, payload: SupplierSchema = Body(...)):
+  supplier = {k: v for k, v in payload.model_dump().items() if v is not None}
+
+  try:
+    if len(supplier) >= 1:
+      update_result = await Supplier.update_one(
+        {"_id": id}, {"$set": supplier}
+      )
+
+      if update_result.modified_count == 0:
+        return JSONResponse(
+          status_code=HttpStatus.NOT_FOUND,
+          data=jsonable_encoder({
+            "msg": f"Supplier with ID {id} not found"
+          })
+        )
+    if (existing_supplier := await Supplier.find_one({"_id": id})) is not None:
+      return JSONResponse(
+          status_code=HttpStatus.NOT_FOUND,
+          data=jsonable_encoder({
+            "msg": f"Supplier with ID {id} updated successfully"
+          })
+        )
   except Exception as e:
     return JSONResponse(
       status_code=HttpStatus.SERVER_ERROR,
