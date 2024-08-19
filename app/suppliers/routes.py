11d1edfa8e01c56cde4f5767af451bc96f3a6f76
johnkatua <johnkatua99@gmail.com>
2024-08-19 14:58:46 +0300

    add a method to delete supplier

--- app/suppliers/routes.py
+++ app/suppliers/routes.py
@@ -70,11 +70,38 @@ async def update_supplier(id: str, payload: SupplierSchema = Body(...)):
     updated_supplier = await Supplier.find_one({"_id": id})
     if updated_supplier:
       return JSONResponse(
-          status_code=HttpStatus.NOT_FOUND,
+          status_code=HttpStatus.OK,
           data=jsonable_encoder({
             "msg": f"Supplier with ID {id} updated successfully"
           })
         )
+  except Exception as e:
+    return JSONResponse(
+      status_code=HttpStatus.SERVER_ERROR,
+      data=jsonable_encoder({
+        "msg": str(e)
+      })
+    )
+
+
+@router.delete('/{id}', response_description="Delete a supplier")
+async def delete_supplier(id: str):
+  try:
+    delete_result = Supplier.delete_one({"id": id})
+    if delete_result.deleted_count == 1:
+      return JSONResponse(
+          status_code=HttpStatus.OK,
+          data=jsonable_encoder({
+            "msg": f"Supplier with ID {id} deleted successfully"
+          })
+        )
+    
+    return JSONResponse(
+      status_code=HttpStatus.NOT_FOUND,
+      data=jsonable_encoder({
+        "msg": f"Supplier with ID {id} not found"
+      })
+    )
   except Exception as e:
     return JSONResponse(
       status_code=HttpStatus.SERVER_ERROR,
