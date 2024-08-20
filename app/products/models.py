2172f426746224e494b415d756e6e73bc76b2251
johnkatua <johnkatua99@gmail.com>
2024-08-20 14:04:03 +0300

    update products model

--- app/products/models.py
+++ app/products/models.py
@@ -10,9 +10,34 @@ class ProductSchema(BaseModel):
   quantity_in_stock: Optional[int] = None
   reorder_level: Optional[int] = None
 
+  class Config:
+    populate_by_name = True
+    json_schema_extra = {
+      "example": {
+        "_id": "0afdb3a9-6ec5-4ac6-b435-e36cd325cae8",
+        "product_name": "Test Product",
+        "unit_price": 600,
+        "supplier_id": "0afdb3a9-6ec5-4ac6-b435-e36cd325cae8",
+        "quantity_in_stock": 8,
+        "reorder_level": 2
+      }
+    }
+
 class UpdateProductSchema(BaseModel):
   product_name: Optional[str] = None
   unit_price: Optional[int] = None
   supplier_id: Optional[str] = None
   quantity_in_stock: Optional[int] = None
   reorder_level: Optional[int] = None
+
+  class Config:
+    populate_by_name = True
+    json_schema_extra = {
+      "example": {
+        "product_name": "Test Product",
+        "unit_price": 600,
+        "supplier_id": "0afdb3a9-6ec5-4ac6-b435-e36cd325cae8",
+        "quantity_in_stock": 8,
+        "reorder_level": 2
+      }
+    }
