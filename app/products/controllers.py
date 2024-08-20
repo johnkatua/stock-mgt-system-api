76984878a1e093ddae5f1cd3e8bca583a6a3140b
johnkatua <johnkatua99@gmail.com>
2024-08-20 14:54:55 +0300

    add a method to get all products

--- app/products/controllers.py
+++ app/products/controllers.py
@@ -3,6 +3,7 @@ from app.payload_util import HttpStatus
 from app.products.models import ProductSchema, UpdateProductSchema
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
+from typing import List
 
 async def create_product(payload: ProductSchema):
   product = jsonable_encoder(payload)
@@ -13,6 +14,21 @@ async def create_product(payload: ProductSchema):
       data=jsonable_encoder({
         "msg": "Product created successfully"
       })
+    )
+  except Exception as e:
+    return JSONResponse(
+      status_code=HttpStatus.CREATED,
+      data=jsonable_encoder({
+        "msg": str(e)
+      })
+    )
+  
+async def list_products() -> List[ProductSchema]:
+  try:
+    products = await list(Product.find(limit=100))
+    return JSONResponse(
+      status_code=HttpStatus.OK,
+      data=products
     )
   except Exception as e:
     return JSONResponse(
