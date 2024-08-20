a1f6245cca3c144d15e56363c0835e595129a509
johnkatua <johnkatua99@gmail.com>
2024-08-20 15:19:33 +0300

    setup product routes

--- app/api.py
+++ app/api.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from dotenv import dotenv_values
 from app.auth import auth_routes
 from app.suppliers.routes import router as supplier_routes
+from app.products.routes import router as product_routes
 from app.auth.auth_bearer import JWTBearer
 
 config = dotenv_values()
@@ -25,6 +26,7 @@ app.add_middleware(
 
 app.include_router(auth_routes.router, tags=['Auth'], prefix='/api/auth')
 app.include_router(supplier_routes, tags=['Suppliers'], prefix='/api/suppliers')
+app.include_router(product_routes, tags=['Products'], prefix='/api/products')
 
 @app.get("/api/healthcheck", dependencies=[Depends(JWTBearer())])
 def root():
