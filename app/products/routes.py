252c0ff2f0b5e4a089f982163c85b84cae38e025
johnkatua <johnkatua99@gmail.com>
2024-08-20 15:15:08 +0300

    add endpoint to delete product

--- app/products/routes.py
+++ app/products/routes.py
@@ -1,6 +1,6 @@
 from app.auth.auth_bearer import JWTBearer
 from app.products.models import ProductSchema, UpdateProductSchema
-from app.products.controllers import create_product, list_products
+from app.products.controllers import create_product, list_products, delete_product
 from fastapi import APIRouter, Body, Depends
 
 router = APIRouter()
@@ -15,10 +15,17 @@ async def create_product_route(payload: ProductSchema = Body(...)):
 
 
 @router.get(
-  "/",
-  response_description="List all products",
-  dependencies=[Depends(JWTBearer)]
-)
+    "/",
+    response_description="List all products",
+    dependencies=[Depends(JWTBearer)]
+  )
 async def list_products_route():
   await list_products()
 
+@router.delete(
+    "/{id}",
+    response_description="Delete a product",
+    dependencies=[Depends(JWTBearer)]
+  )
+async def delete_product_route(id: str):
+  await delete_product(id)
