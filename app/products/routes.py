5560b5e2ed1858c2d2c4aac4d77449b174371b48
johnkatua <johnkatua99@gmail.com>
2024-08-20 15:23:23 +0300

    fix authorization dependency

--- app/products/routes.py
+++ app/products/routes.py
@@ -17,7 +17,7 @@ async def create_product_route(payload: ProductSchema = Body(...)):
 @router.get(
     "/",
     response_description="List all products",
-    dependencies=[Depends(JWTBearer)]
+    dependencies=[Depends(JWTBearer())]
   )
 async def list_products_route():
   await list_products()
@@ -25,7 +25,7 @@ async def list_products_route():
 @router.delete(
     "/{id}",
     response_description="Delete a product",
-    dependencies=[Depends(JWTBearer)]
+    dependencies=[Depends(JWTBearer())]
   )
 async def delete_product_route(id: str):
   await delete_product(id)
