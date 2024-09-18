75a05a875a5620fc9e4e1b4ebeb31f8b3524f2f7
Onesmus Wambugu <onesmus1024@gmail.com>
2024-09-18 17:52:18 +0300

    fix : remove secrets

--- app/database.py
+++ app/database.py
@@ -3,8 +3,12 @@ from pymongo.mongo_client import MongoClient
 import os
 
 config = dotenv_values("./.env")
+# MONGO_URI="mongodb+srv://johnkatua:sIlzKWnBRkxOWAPq@cluster0.b4ovhgj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
-mongo_uri = os.getenv("MONGO_URI")
+MONGO_PASS = os.getenv("MONGO_PASS")
+MONGO_USER = os.getenv("MONGO_USER")
+MONGO_CLUSTER = os.getenv("MONGO_CLUSTER")
+mongo_uri = f"mongodb+srv://{MONGO_USER}:{MONGO_PASS}@{MONGO_CLUSTER}/?retryWrites=true&w=majority&appName=Cluster0"
 database = os.getenv("DATABASE")
 
 client = MongoClient(mongo_uri)
