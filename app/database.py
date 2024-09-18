0a9c427e3c0bc6b27d1333e8dd90060aadf8e1c1
Onesmus Wambugu <onesmus1024@gmail.com>
2024-09-18 12:46:10 +0300

    fix : secrets

--- app/database.py
+++ app/database.py
@@ -1,10 +1,11 @@
 from dotenv import dotenv_values
 from pymongo.mongo_client import MongoClient
+import os
 
 config = dotenv_values("./.env")
 
-mongo_uri = config["MONGO_URI"]
-database = config["DATABASE"]
+mongo_uri = os.getenv("MONGO_URI")
+database = os.getenv("DATABASE")
 
 client = MongoClient(mongo_uri)
 db = client[database]
